c213a579d150673949f4d58065836563d28b990e
Fazalmahm00d <fazalmah000@gmail.com>
2024-11-24 14:32:01 +0530

    fixed bugs in authentication

--- app/components/login-components/loginFirst.js
+++ app/components/login-components/loginFirst.js
@@ -18,6 +18,8 @@ function LoginFirst(){
     const [isSendingReq,setIsSendingReq]=useState(false);
     const[isValidMail,setIsValidMail]=useState(true);
     const[IsValidPsswd,setIsValidPsswd]=useState(true);
+
+    const[isType,setIsType]=useState("password")
     const signUpURL="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAnCRZfZTUHUPdYrWGjYPV7PSstRIKboSM";
     const logInURL="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAnCRZfZTUHUPdYrWGjYPV7PSstRIKboSM"
 
@@ -76,9 +78,11 @@ function LoginFirst(){
             password,
             returnSecureToken:true
         }
-        console.log(isValidMail,"email valid or not");
-        console.log(IsValidPsswd,"passwd valid or not")
+        // console.log(isValidMail,"email valid or not");
+        // console.log(IsValidPsswd,"passwd valid or not")
         if(pr && pr2){
+            setIsValidMail(true);
+            setIsValidPsswd(true);
             authFirebase(data);
         }
     }
@@ -90,6 +94,14 @@ function LoginFirst(){
     }
     function switchToSignUp(){
         setIsLogin(false);
+    }
+    function changeType(e){
+        e.preventDefault();
+        setIsType("text")
+    }
+    function switchType(e){
+        e.preventDefault();
+        setIsType("password")
     }
     return(
         <div className="flex w-full h-[100vh] p-10 bg-[#F9F1E7]">
@@ -112,18 +124,31 @@ function LoginFirst(){
                     <form onSubmit={handleSubmit}  className="flex flex-col gap-3 w-full ">
                         <label className='mt-4 text-[#B88E2F] font-bold' htmlFor="email">Email</label>
                         {!isValidMail ? 
-                        <div><input  className="focus:outline-none text-md px-8 py-3 rounded-3xl border-2 border-red-700 " id="email" name="email" type="text" placeholder="Enter your Email Address"/><p className='text-red-700'>Please enter a valid email address</p></div> : <input className="focus:outline-none text-md px-8 py-3 rounded-3xl " id="email" name="email" type="text" placeholder="Enter your Email Address"/>}
+                        <div><input  className="focus:outline-none text-md px-8 py-3 rounded-3xl border-2 border-red-700 placeholder:text-red-700 " id="email" name="email" type="text" placeholder="Enter your Email Address"/><p className='text-red-700'>Please enter a valid email address</p></div> : <input className="focus:outline-none text-md px-8 py-3 rounded-3xl " id="email" name="email" type="text" placeholder="Enter your Email Address"/>}
                         <label className='mt-4 text-[#B88E2F] font-bold' htmlFor="password">Password</label>
+                        <div className='flex justify-between w-full px-8 py-3 bg-white rounded-3xl'>
                         {
-                        !IsValidPsswd?<div><input className="focus:outline-none text-md px-8 py-3 rounded-3xl" id="password" name="password" type="password" placeholder="Enter your Password"/><p className='text-red-700'>Password  Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters</p></div>
-                        :<input className="text-md px-8 py-3 rounded-3xl" id="password" name="password" type="password" placeholder="Enter your Password"/>
+                        !IsValidPsswd?<div><input className="focus:outline-none text-md   border-2 border-red-700 placeholder:text-red-700" id="password" name="password" type="password" placeholder="Enter your Password"/><p className='text-red-700'>Password  Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters</p></div>
+                        :<input className="text-md focus:outline-none h-full  " id="password" name="password" type={isType} placeholder="Enter your Password"/>
                         }
+                        {isType==="password" ?<button onClick={changeType}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                        </svg></button>:<button onClick={switchType}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+                            <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
+                            <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
+                            <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
+                            </svg>
+                        </button>
+                        }
+                        </div>
                         <div className="flex justify-between items-center">
                         <div className='flex items-center'>
-                        <input  type="checkbox" checked={isRemember} onChange={(e) => setIsRemember(e.target.checked)} name="remember"/>
+                        <input  className='cursor-pointer' type="checkbox" checked={isRemember} onChange={(e) => setIsRemember(e.target.checked)} name="remember"/>
                         <p className="ml-2 text-sm text-[#B88E2F]">Remember Me</p>
                         </div>
-                        <div onClick={forgotHandler} className="text-sm text-[#B88E2F] font-bold">Forgot Password?</div>
+                        <div onClick={forgotHandler} className="cursor-pointer text-sm text-[#B88E2F] font-bold ">Forgot Password?</div>
                     </div>
                     <div className='flex justify-end'>
                         {
